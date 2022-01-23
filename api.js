eba0212c9ab578e22ab1c7c3a92266a2004e5159
Yakow Azrilevich <svetazril@gmail.com>
2022-01-23 20:01:59 +0200

    did it to show json in console2

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-let github_link ='https://young-tea.github.io/hypixel-crafts-api/';
+let github_link ='https://young-tea.github.io/hypixel-crafts-api/crafts.json';
 $.getJSON(github_link, function(data) {
      //data is the JSON string
      console.log(data);
